de2c15908e4f9c7a863dd8f652dd1539717a2f33
SeanyC <sean.collier928@yahoo.com>
2023-11-19 15:25:33 -0800

    local storage for multiple choice and fill in the blank questions, answer validation as an alert on those two questions.

--- script.js
+++ script.js
@@ -47,8 +47,13 @@ document.addEventListener('DOMContentLoaded', function() {
                 console.log("Raw data from Firebase:", data); // Log raw data
 
                 if (data) {
-                    quizQuestions = Object.values(data);
-                    randomQuestions(quizQuestions);
+                    // Convert the object into an array and include the question keys as IDs
+                    quizQuestions = Object.entries(data).map(([id, questionData]) => ({
+                        id,
+                        ...questionData
+                    }));
+
+                    randomQuestions(quizQuestions); // Shuffle the questions if needed
                     console.log("Fetched Questions:", quizQuestions);
                     initQuiz();
                 } else {
@@ -58,6 +63,7 @@ document.addEventListener('DOMContentLoaded', function() {
             })
             .catch(error => console.error("Error fetching data: ", error));
     }
+
 
     // Initialize quiz
     function initQuiz() {
@@ -82,8 +88,24 @@ document.addEventListener('DOMContentLoaded', function() {
                 break;
             default:
                 displayMultipleChoiceQuestion(question);
+
+        }
+        //show or hide the submit button
+        const submitButton = document.querySelector('.nav-button.submit');
+        if (submitButton) {
+        if (index === quizQuestions.length - 1) {
+            submitButton.style.display = 'block';
+        } else {
+            submitButton.style.display = 'none';
         }
     }
+
+    }
+
+    document.querySelector('.nav-button.submit').addEventListener('click', function() {
+        validateAnswers();
+        localStorage.removeItem('quizProgress');
+    });
 
     function displayMultipleChoiceQuestion(question) {
         clearPreviousQuestionDisplay();
@@ -118,11 +140,33 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function displayFillInTheBlankQuestion(question) {
         clearPreviousQuestionDisplay();
+
+        // Display the question
+        const questionsArea = document.querySelector('.question-area');
+        questionsArea.innerHTML = question.text;
+
+        // Find the fill-in-the-blank container and input field
         const fillInTheBlankContainer = document.querySelector('.fill-in-the-blank');
-        const questionText = document.querySelector('.question-area');
-        questionText.textContent = question.text;
+        const blankInput = document.querySelector('.blank-input');
+
+        // Remove existing event listeners from the input field
+        const newInput = blankInput.cloneNode(true);
+        fillInTheBlankContainer.replaceChild(newInput, blankInput);
+
+        // Update the input field value based on the stored answer or reset it
+        newInput.value = userAnswers[question.id] || '';
+
+        // Show the fill-in-the-blank container
         fillInTheBlankContainer.style.display = 'block';
+
+        // Update the stored answer whenever the input changes
+        newInput.addEventListener('input', function() {
+            userAnswers[question.id] = newInput.value;
+            localStorage.setItem('quizProgress', JSON.stringify({ currentQuestionIndex, userAnswers }));
+        });
     }
+
+
 
     function displayOrderingQuestion(question){
         clearPreviousQuestionDisplay();
@@ -218,7 +262,7 @@ document.addEventListener('DOMContentLoaded', function() {
     };
 
     function clearPreviousQuestionDisplay() {
-        const questionArea = document.querySelectorAll('.question-area');
+        const questionArea = document.querySelector('.question-area');
         if (questionArea) {
             questionArea.innerHTML = '';
         }
@@ -232,23 +276,43 @@ document.addEventListener('DOMContentLoaded', function() {
     let userAnswers = {};
 
     function storeAnswer(questionId, answer) {
-        userAnswers[questionId] = answer
-    }
-
-    function validateAnswers() {
-        let correctCount = 0;
-        quizQuestions.forEach(question => {
-            if (userAnswers[question.id] === question.correctAnswer) {
-                correctCount++
-            }
-            displayResults(correctCount);
-        });
+        userAnswers[questionId] = answer;
+        console.log("storing answers:", questionId, answer);
+        localStorage.setItem('quizProgress', JSON.stringify({currentQuestionIndex, userAnswers}));
     };
 
-    function displayResults(correctCount) {
+    function loadQuizProgress() {
+        const progress = JSON.parse(localStorage.getItem('quizProgress'));
+        if (progress) {
+            currentQuestionIndex = progress.currentQuestionIndex;
+            userAnswers = progress.userAnswers;
+            displayQuestion(currentQuestionIndex);
+            updateProgressBar();
+        } else {
+            displayQuestion(0);
+        }
+    }
+
+    document.addEventListener('DOMContentLoaded', loadQuizProgress);
+
+    function validateAnswers() {
+        // if (Object.keys(userAnswers).length < quizQuestions.length) {
+        //     alert("answer all questions to submit")
+        //     return;
+        // }
+
+        let correctCount = 0;
+        quizQuestions.forEach(question => {
+            const userAnswer = userAnswers[question.id];
+            const correctAnswer = question.answer;
+            console.log(`Question ID: ${question.id}, User Answer: ${userAnswer}, Correct Answer: ${correctAnswer}`)
+            if (userAnswer === correctAnswer) {
+                correctCount++
+            }
+        });
         const totalQuestions = quizQuestions.length;
         alert(`You got ${correctCount} out of ${totalQuestions} questions correct.`);
-    }
+    };
 
     // Update the progress bar
     function updateProgressBar() {
